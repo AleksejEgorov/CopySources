b3def78e10f763ada7d52f1381ce7572a9040e1a
Aleksej Egorov <Egorov.Aleksej.V@outlook.com>
2023-01-02 14:18:35 +0300

    Created.

---
+++ Updater.ps1
@@ -0,0 +1,28 @@
+[void][System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
+
+if (
+    (Test-Path ([System.IO.Path]::Combine($PSScriptRoot,'.git'))) -and
+    (& {try {git} catch {$false}})
+) {
+    Set-Location $PSScriptRoot
+    git pull --rebase
+    $Message = "Updated from git repository"
+}
+else {
+    $ZipLink = 'https://github.com/AleksejEgorov/CopySources/archive/refs/heads/master.zip'
+    $ArchFile = [System.IO.Path]::Combine($env:TEMP,'CopySources-master.zip')
+    Invoke-WebRequest -Uri $ZipLink -OutFile $ArchFile
+    Expand-Archive -Path $ArchFile -DestinationPath $env:TEMP -Force
+    Copy-Item -Path [System.IO.Path]::Combine($env:TEMP,'CopySources-master','*') -Destination $PSScriptRoot -Recurse -Force
+    $Message = "Updated from downloaded zip file"
+}
+
+$Form = [System.Windows.Forms.Form]::new()
+$Form.TopMost = $true
+[System.Windows.Forms.MessageBox]::Show(
+    $Form,
+    $Message,
+    'Copy sources',
+    'OK',
+    'Info'
+)
